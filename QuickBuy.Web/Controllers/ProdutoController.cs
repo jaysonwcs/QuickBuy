5639e07232ec87e4bb8238294ed34a64bfd43600
Jayson Silva <jaysonwcs@gmail.com>
2020-06-29 18:45:27 -0300

    Added controller

--- QuickBuy.Web/Controllers/ProdutoController.cs
+++ QuickBuy.Web/Controllers/ProdutoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,35 @@ namespace QuickBuy.Web.Controllers
         public ProdutoController(IProdutoRepositorio produtoRepositorio)
         {
             _produtoRepositorio = produtoRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_produtoRepositorio.ObterTodos());
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Produto produto)
+        {
+            try
+            {
+                _produtoRepositorio.Adicionar(produto);
+                return Created("api/produto", produto);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.ToString());
+            }
         }
     }
 }
